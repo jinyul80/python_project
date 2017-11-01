5648889aa8a12fef30c17600c56ad56a36f178be
jumble80 <jumble80@naver.com>
2017-11-01 09:08:16 +0900

    TF Total parameter 출력 방법

---
+++ tensorflow/print_total_parameter.py
@@ -0,0 +1,54 @@
+import tensorflow as tf
+import numpy as np
+
+def case1():
+    cc = np.sum([np.prod(v.shape) for v in tf.trainable_variables()])
+    print('total parameter :', cc)
+
+    sess.close()
+
+def case2():
+    # All trainable variables
+    val_list1 = tf.trainable_variables()
+
+    # Global scope trainable variables
+    # val_list2 = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'global')
+
+    total_parameters = 0
+    for variable in val_list1:
+        # shape is an array of tf.Dimension
+        shape = variable.get_shape()
+        # print(shape)
+        # print(len(shape))
+        variable_parameters = 1
+        for dim in shape:
+            # print(dim)
+            variable_parameters *= dim.value
+        print(variable, ":", variable_parameters)
+        total_parameters += variable_parameters
+    print(total_parameters)
+
+
+# main
+aa = np.random.random([1, 5])
+bb = [5]
+
+X = tf.placeholder(tf.float32, shape=[None, 5])
+Y = tf.placeholder(tf.float32, shape=[None])
+
+net = tf.layers.dense(inputs=X, units=20)
+pred = tf.layers.dense(inputs=net, units=1)
+
+cost = tf.reduce_mean(tf.square(pred - Y))
+
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
+train = optimizer.minimize(cost)
+
+# Launch the graph in a session.
+sess = tf.Session()
+# Initializes global variables in the graph.
+sess.run(tf.global_variables_initializer())
+
+case1()
+case2()
+
