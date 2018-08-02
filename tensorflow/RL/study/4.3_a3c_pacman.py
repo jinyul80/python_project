195d363ea721ea0d72c957d8cec31a57b15b4e3e
jumble80 <jumble80@naver.com>
2018-08-02 10:45:47 +0900

    TF RL - A3C에 Batch normalization 적용 - learning_rate 상향 조정

--- tensorflow/RL/study/4.3_a3c_pacman.py
+++ tensorflow/RL/study/4.3_a3c_pacman.py
@@ -11,8 +11,8 @@ import mini_pacman
 
 parser = argparse.ArgumentParser(description="Simple 'argparse' demo application")
 parser.add_argument('--mode', default='train', help='Execute mode')
-parser.add_argument('--learning_rate', default=1e-3, type=float)
-parser.add_argument('--logdir', default='./log/4.3_a3c_pacman_log')
+parser.add_argument('--learning_rate', default=1e-2, type=float)
+parser.add_argument('--logdir', default='./log/4.3_a3c_pacman_log/a3c+BN+lr')
 parser.add_argument('--max_steps', default=1000001, type=int)
 parser.add_argument('--n_threads', default=4, type=int)
 parser.add_argument('--update_ep_size', default=1, type=int)
@@ -69,28 +69,30 @@ class A3CNetwork(object):
         self._build()
 
         self.global_episodes = tf.Variable(0, dtype=tf.int32, name='global_episodes', trainable=False)
+        self.input_ep = tf.placeholder(dtype=tf.int32)
+        self.update_ep = self.global_episodes.assign(self.input_ep)
 
     def _build(self):
         with tf.variable_scope(self.name):
-            # 신경망은 게임으로부터 벡터화된 배열로 프레임을 받아서
-            # 이것을 리사이즈 하고, 4개의 콘볼루션 레이어를 통해 처리한다.
-
-            # 입력값을 받는 부분 21168 차원은 84*84*3 의 차원이다.
+            # 입력값을 받는 부분
             self.states = tf.placeholder(shape=[None, self.input_size], dtype=tf.float32, name='input_state')
             self.actions = tf.placeholder(shape=[None, self.output_size], dtype=tf.float32, name='input_action')
             self.advantages = tf.placeholder(tf.float32, [None], name="advantages")
             self.rewards = tf.placeholder(tf.float32, [None], name="reward")
 
-            # conv2d 처리를 위해 84x84x3 으로 다시 리사이즈
             _imageIn = tf.reshape(self.states, shape=[-1, *self.input_shape])
 
             # Dense 레이어
-            _dense = tf.layers.dense(inputs=_imageIn, units=128, activation=tf.nn.relu)
-            _dense = tf.layers.dense(inputs=_dense, units=64, activation=tf.nn.relu)
+            net = tf.layers.dense(inputs=_imageIn, units=128, activation=tf.nn.relu)
+            net = tf.layers.dense(inputs=net, units=64)
+
+            # Normalization
+            net = tf.layers.batch_normalization(inputs=net)
+            net = tf.nn.relu(net)
 
             # LSTM
             _rnn_out_size = 32
-            _rnn_in = tf.expand_dims(_dense, [0])
+            _rnn_in = tf.expand_dims(net, [0])
             lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(num_units=_rnn_out_size, state_is_tuple=True)
             # cell 초기화
             c_init = np.zeros((1, lstm_cell.state_size.c), np.float32)
@@ -110,12 +112,16 @@ class A3CNetwork(object):
                 time_major=False, dtype=tf.float32)
             lstm_c, lstm_h = lstm_state
             self.state_out = (lstm_c[:1, :], lstm_h[:1, :])
+
             # 최종을 다시 벡터화
-            rnn_out = tf.reshape(lstm_outputs, [-1, _rnn_out_size])
+            net = tf.reshape(lstm_outputs, [-1, _rnn_out_size])
+
+            # Normalization
+            net = tf.layers.batch_normalization(inputs=net)
 
             # Output
-            self.pred = tf.layers.dense(inputs=rnn_out, units=self.output_size, activation=tf.nn.softmax, name='pred')
-            self.values = tf.squeeze(tf.layers.dense(inputs=rnn_out, units=1, name="values"))
+            self.pred = tf.layers.dense(inputs=net, units=self.output_size, activation=tf.nn.softmax, name='pred')
+            self.values = tf.squeeze(tf.layers.dense(inputs=net, units=1, name="values"))
 
             # Loss 계산
             _policy_gain = -tf.reduce_sum(tf.log(self.pred + 1e-5) * self.actions, axis=1) * self.advantages
@@ -179,9 +185,9 @@ class Agent(threading.Thread):
         self.is_training = is_training
 
     # Episode 결과 출력
-    def print(self, reward, avg_reward, rTime):
-        message = "Episode : {} , Agent(name={}, reward= {:.2f}, avg= {:.2f}) ({:.5f} sec)".format(self.local_episode,
-                                                     self.name, reward, avg_reward, rTime)
+    def print(self, reward, avg_reward, frame_sec):
+        message = "Episode : {} , Agent(name={}, reward= {:.2f}, avg= {:.2f}) ({:.2f} sec)".format(self.local_episode,
+                                                     self.name, reward, avg_reward, frame_sec)
         print(message)
 
     # State 전처리
@@ -265,9 +271,10 @@ class Agent(threading.Thread):
 
         # 러닝 시간
         duration = time.time() - start_time
-        sec_per_step = float(duration) / episode_step
+        frame_sec = episode_step / float(duration + 1e-6)
 
-        self.print(np.mean(reward_list), avg_reward, sec_per_step)
+
+        self.print(np.mean(reward_list), avg_reward, frame_sec)
 
     # Model 학습
     def train(self, buffer):
@@ -360,16 +367,25 @@ def main_train():
             init = tf.global_variables_initializer()
             sess.run(init)
 
+            # saver 설정
+            var_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, "global")
+            saver = tf.train.Saver(var_list=var_list)
+
             # Save 파일 있을 경우 복구
-            if tf.train.get_checkpoint_state(os.path.dirname(save_path)):
-                var_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, "global")
-                saver = tf.train.Saver(var_list=var_list)
-                saver.restore(sess, save_path)
+            if tf.train.get_checkpoint_state(checkpoint_dir):
+                read_path = tf.train.latest_checkpoint(checkpoint_dir)
+                saver.restore(sess, read_path)
 
                 Agent.global_episode = sess.run(global_network.global_episodes)
                 print("Model restored to global")
             else:
                 print("No model is found")
+
+            # 모델 그래프 최종 확정
+            tf.get_default_graph().finalize()
+
+            print('\nProgram start')
+            print('Learning rate :', args.learning_rate)
 
             for t in thread_list:
                 t.start()
@@ -377,21 +393,18 @@ def main_train():
 
             print("Ctrl + C to close")
 
-            # saver 설정
-            var_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, "global")
-            saver = tf.train.Saver(var_list=var_list)
-
             div_num = 1000
             save_idx = int(sess.run(global_network.global_episodes) / div_num)
             while not coord.should_stop():
                 current_episode = Agent.global_episode
-                sess.run(global_network.global_episodes.assign(current_episode))
+                sess.run(global_network.update_ep, feed_dict={global_network.input_ep: current_episode})
+
                 temp_idx = int(current_episode / div_num)
                 # Global step XX 회마다 모델 저장
                 if save_idx != temp_idx:
                     save_idx = temp_idx
 
-                    saver.save(sess, save_path)
+                    saver.save(sess, save_path, global_step=current_episode)
                     print('Checkpoint Saved to {}'.format(save_path))
 
                 if current_episode >= args.max_steps:
@@ -400,7 +413,7 @@ def main_train():
                     coord.join(thread_list)
 
                 # 평균 300점 이상이면 종료
-                if np.mean(Agent.global_reward_list) > 300:
+                if np.mean(Agent.global_reward_list) > 390:
                     coord.request_stop()
                     coord.join(thread_list)
 
@@ -418,7 +431,8 @@ def main_train():
                 pass
 
     finally:
-        saver.save(sess, save_path)
+        current_episode = Agent.global_episode
+        saver.save(sess, save_path, global_step=current_episode)
         print('Checkpoint Saved to {}'.format(save_path))
 
         sess.close()
